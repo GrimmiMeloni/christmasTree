13472f86cee4db4bfafcc944256d175c17deebca
Michael Hess <m_hess@gmx.net>
2017-12-20 21:12:30 +0100

    initial commit

---
+++ src/RandomOrnaments.ino
@@ -0,0 +1,78 @@
+/**
+ * An example demonstrating the christmasTree library
+ * The example exposes some of the library's interface
+ * Specifically, the example shows how to create an instance
+ * of the christmasTree library, initialize the instance,
+ * set the ornaments, and refresh the matrix
+ *
+ * The library is based on the NeoMatrix modules from Adafruit
+ * You can use the NeoMatrix modules as ornaments on your christmas tree
+ *
+ * ** Happy Holidays **
+ */
+
+// Includes the necessary libraries
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include "christmasTree.h"
+
+#define PIN 6  // Pin to which the input of the matrix is connected
+#define PERIOD 10000  // Refreshes the ornament every 10 sec
+
+#define BUTTON1 2 //first button
+#define BUTTON2 8 //first button
+
+long time_ref;  // Used as a time reference
+
+// Creates a christmasTree instance with the DOUT pin as an argument
+christmasTree ornament(PIN);
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(BUTTON1, INPUT_PULLUP);
+    pinMode(BUTTON2, INPUT_PULLUP);
+
+    Serial.begin(9600);
+    Serial.println("start");
+
+    ornament.begin(3);  // Initializes the instance
+    
+    // Selects which ornaments to appear
+    // Available choices: SNOW_BACK, SNOW_FRONT, TREE, GIFT, CHILD, HOUSE, SPACE_INVADER, RANDOM, CANDY
+    ornament.setOrnaments(SNOW_BACK + SPACE_INVADER);  
+    
+    time_ref = millis();  // Gets the temporal baseline
+}
+
+void loop()
+{
+    if ( (unsigned long) (millis() - time_ref) > PERIOD )
+    {
+        // Picks an ornament at random.
+        // It's a bit esoteric, but basically setting a bit in a byte enables a specific ornament
+        uint8_t new_ornament = 0x02 << ( rand() % 7 );
+
+        ornament.setOrnaments(SNOW_BACK + new_ornament);  // Sets the new ornament
+        time_ref = millis();
+    }
+    
+    // Refreshes the drawing on the matrix.
+    // Any change basically involves scrolling the snowflakes down (if enabled)
+    ornament.refresh();
+    
+    digitalWrite(LED_BUILTIN, HIGH);
+    int button1 = digitalRead(BUTTON1);
+    int button2 = digitalRead(BUTTON2);
+
+    if (button1 == LOW)
+        Serial.println("Button1 pressed");
+
+    if (button2 == LOW)
+        Serial.println("Button2 pressed");
+
+    delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(300);
+}
---
+++ src/christmasTree.h
@@ -0,0 +1,63 @@
+/**
+ * Name: christmasTree
+ * Author: Nick Lamprianidis (paign10.ln@gmail.com)
+ * Version: 1.0
+ * Description: A library with some ornaments for decorating a christmas tree
+                The library is based on the NeoMatrix modules
+ * License: Copyright (c) 2013 Nick Lamprianidis 
+ *          This library is licensed under the MIT license
+ *          http://www.opensource.org/licenses/mit-license.php
+ *
+ * Filename: christmasTree.h
+ * File description: Definitions and methods for the christmasTree library
+ */
+
+#ifndef christmasTree_h
+#define christmasTree_h
+
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Arduino.h>
+
+// Available ornaments
+enum ORNAMENTS
+{
+    SNOW_BACK = 0x01,
+    TREE = 0x02,
+    GIFT = 0x04,
+    CHILD = 0x08,
+    HOUSE = 0x10,
+    SPACE_INVADER = 0x20,
+    RANDOM = 0x40,
+    CANDY = 0x80,
+    SNOW_FRONT = 0x100
+};
+
+// Class for the creation of ornaments
+class christmasTree
+{
+public:
+    christmasTree(uint8_t pin);
+    void begin(uint8_t brightness = 5);
+    void setBrightness(uint8_t brightness);
+    void setOrnaments(uint16_t ornaments);
+    void refresh();
+    void refresh(uint16_t ornaments);
+
+private:
+    Adafruit_NeoMatrix *matrix;
+    uint8_t snowPixels[8];
+    uint16_t _ornaments;  // Individual set bits get specific ornaments drawn
+    void dropSnow();
+    void drawTree();
+    void drawGift();
+    void drawChild();
+    void drawHouse();
+    void drawSpaceInvader();
+    void drawRandom();
+    void drawCandy();
+    void drawRainbow();
+};
+
+#endif  // christmasTree
